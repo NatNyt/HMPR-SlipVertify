45e8b73bb3ef219216fb0051e7970b1e88324dcd
NatNyt <70100268+NatNyt@users.noreply.github.com>
2023-05-05 21:30:31 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -17,25 +17,65 @@ function decodeQrCode(image: Jimp): Promise<string> {
   });
 }
 
-async function getSlipData(apiKey: string, path: string): Promise<any> {
-  const buffer = fs.readFileSync(path);
-  const image = await Jimp.read(buffer);
-  const data = await decodeQrCode(image);
-  if(!data || data.trim().length == 0){
-    throw new qrCodeInvalid("QRcoode is not a valid")
-  }
-  const result = await axios.post('https://slipverify.hmpr.xyz/api/api1/apislip', {
-    "api_key": apiKey,
-    "qrcode": data
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'HMPRSLIPAPI'
+export async function getSlipDataRaw(apiKey: string, path: string): Promise<any> {
+    const buffer = fs.readFileSync(path);
+    const image = await Jimp.read(buffer);
+    const data = await decodeQrCode(image);
+    if(!data || data.trim().length == 0){
+      throw new qrCodeInvalid("QRcoode is not a valid")
     }
-  });
-  
-  const body: ApiResult = result.data
-  return body
-}
-
-export default getSlipData;
+    const result = await axios.post('https://slipverify.hmpr.xyz/api/api1/apislip', {
+      "api_key": apiKey,
+      "qrcode": data
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'HMPRSLIPAPI'
+      }
+    });
+    
+    const body: ApiResult = result.data
+    return body
+  };
+export default async function getSlipData(apiKey: string, path: string): Promise<any> {
+    const buffer = fs.readFileSync(path);
+    const image = await Jimp.read(buffer);
+    const data = await decodeQrCode(image);
+    if(!data || data.trim().length == 0){
+      throw new qrCodeInvalid("QRcoode is not a valid")
+    }
+    const result = await axios.post('https://slipverify.hmpr.xyz/api/api1/apislip', {
+      "api_key": apiKey,
+      "qrcode": data
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'HMPRSLIPAPI'
+      }
+    });
+    
+    const body: ApiResult = result.data
+    if(body.data == null) {
+        const respone = {
+            status: (body.data == null) ? false : true,
+        }
+        return respone
+    }
+    const respone = {
+        status: (body.data == null) ? false : true,
+        transferRef: body.data?.verifySlipInfo?.transRef,
+        date: body.data?.verifySlipInfo?.transDate,
+        amount: body.data?.verifySlipInfo?.transAmount,
+        sender: {
+            bank: body.data?.verifySlipInfo?.fromBankName,
+            name: body.data?.verifySlipInfo?.fromAccountNameTh,
+            accountNumber: body.data?.verifySlipInfo?.fromAccountNo
+        },
+        receiver: {
+            bank: body.data?.verifySlipInfo?.toBankName,
+            name: body.data?.verifySlipInfo?.toAccountNameTh,
+            accountNumber: body.data?.verifySlipInfo?.toAccountNo
+        }
+    }
+    return respone
+};
